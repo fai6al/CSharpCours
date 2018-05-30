3c61046a63bee03fde682cc2d7aa41767f76f839
Faisal S. Alharbi <falharbi@seu.edu.sa>
2018-05-30 14:08:09 +0300

    Add project files.

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Project 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("heloo Faisal  ");
+            Console.ReadKey();
+
+        }
+    
+}
